167dfc2ce4b9ca9370ede668f84fe69ddbcdcda9
Shayan_Ta <66938889+ShaShaTa47@users.noreply.github.com>
2024-12-13 02:00:20 +1300

    redirect home to challenges

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 import CodeEditor from "./components/ide/ide";
 import Link from 'next/link';
 import ChallengesPage from "./challenges/page";
-
+import { redirect } from 'next/navigation'
 
 export default function Home() {
+
+  redirect("/challenges")
   return (
     null
     /*<ChallengesPage/>*/
